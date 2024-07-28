ba79dc9b21d6bf12e95e75c21d3d4950ff5ce02f
ziweiren <56014191+ziweiren@users.noreply.github.com>
2024-07-28 00:13:47 -1000

    finish

--- main.py
+++ main.py
@@ -5,19 +5,11 @@
 
 import pandas as pd
 
-
-def match(df, match_sku):
-    sku = df["sku"]
-    sku = sku.split("*")[0]
-    return int(match_sku[sku])
-
-
 # Replace 'your_file.xls' with the path to your XLS file
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    match_sku = {'135156123156': '457852133', '156351781123': '567890123'}
     out_storage = 'out_storage.xlsx'
     # sheet_name = '订单模板'
 
@@ -28,20 +20,16 @@ if __name__ == '__main__':
     df_out_temu = pd.read_excel(out_temu)  # temu
 
     # Select and rename the columns you want to combine
-    df_temu_selected = df_out_temu[
-        ["订单号", "子订单号", "货品SPU ID", "商品件数", "收货人姓名", "收货人联系方式", "详细地址1"]]
-    df_storage_selected = df_out_storage[["跟踪号", "派送渠道", "收件人", "收件人电话", "收件地址1", "产品明细"]]
+    df_temu_selected = df_out_temu[["订单号", "子订单号", "商品件数", "货品SKU ID"]]
+    df_storage_selected = df_out_storage[
+        ["跟踪号", "派送渠道", "客户订单号"]]
 
     # Rename columns to match the desired order
-    df_temu_selected.rename(columns={"货品SPU ID": "sku", "收货人姓名": "收件人",
-                                     "收货人联系方式": "收件人电话", "详细地址1": "收件人地址"}, inplace=True)
-    df_storage_selected.rename(columns={"跟踪号": "跟踪单号", "派送方式": "物流承运商",
-                                        "收件地址1": "收件人地址", "产品明细": "sku"}, inplace=True)
-
-    df_storage_selected["sku"] = df_storage_selected.apply(lambda r: match(r, match_sku), axis=1)
+    df_temu_selected.rename(columns={"货品SKU ID": "商品SKUID"}, inplace=True)
+    df_storage_selected.rename(columns={"跟踪号": "跟踪单号", "派送方式": "物流承运商", "客户订单号": "订单号"}, inplace=True)
 
     combined_df = pd.merge(df_temu_selected, df_storage_selected,
-                           on=["sku", "收件人电话", "收件人", "收件人地址"],
+                           on=["订单号"],
                            how="inner")
 
     # Combine the selected columns into one dataframe
